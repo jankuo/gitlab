c8944a76096bcba7d25d500548966d1926b93929
sense <llh110220@gmail.com>
2014-04-17 07:48:28 +0800

    skyet demo

--- server.go
+++ server.go
@@ -1,39 +1,37 @@
 package main
 
 import (
+	"github.com/madari/goskirt"
 	"os"
-	"strings"
-	"text/template"
 )
-
-// A simple template definition to test our function.
-// We print the input text several ways.
-//  - the original
-//  - title-cased
-//  - title-cased and then printed whith %q
-//  - printed with %q and then title-cased.
-const templateText = `
-Input: {{ printf "%q" . }}
-Output 0 : {{ title . }}
-Output 1 : {{ title . | printf "%q " }}
-Output 2 : {{ printf "%q" . | title }}
-`
 
 func main() {
 
-	//First we create a FuncMap with to register the function.
-	funcMap := template.FuncMap{
-		//The name "Title" is what function will be called in the template
-		"title": strings.Title,
+	data := `
+
+#this is a title
+
+##this is second title
+
+1. me
+2. he
+3. they
+
++ me
++ hi
++ me
+
+      var me = your
+
+
+
+[me]("http://www.google.com")
+`
+
+	skirt := goskirt.Goskirt{
+		goskirt.EXT_AUTOLINK | goskirt.EXT_STRIKETHROUGH,
+		goskirt.HTML_SMARTYPANTS | goskirt.HTML_USE_XHTML,
 	}
 
-	t, err := template.New("titleTest").Funcs(funcMap).Parse(templateText)
-	if err != nil {
-		panic(err)
-	}
-
-	err = t.Execute(os.Stdout, "the go programing language")
-	if err != nil {
-		panic(err)
-	}
+	skirt.WriteHTML(os.Stdout, []byte(data))
 }
